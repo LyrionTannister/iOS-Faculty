b538c5289de1ce8ad96dc3def78a9b9523923b79
Roman Nikishin <loudpiper@gmail.com>
2020-02-20 20:58:44 +0300

    Пустой проект для домашнего задания №3

---
+++ 1 month/Lesson03/3l_NikishinRoman/3l_NikishinRoman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  3l_NikishinRoman
+//
+//  Created by Admin on 20.02.2020.
+//  Copyright © 2020 Nikishin Roman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
