03b29e4c176782d8e0d9503cce5ecc847f4e7238
Roman Nikishin <loudpiper@gmail.com>
2020-02-24 21:23:43 +0300

    New template for #04 homework created in new branch

---
+++ 1 month/Lesson04/4l_NikishinRoman/4l_NikishinRoman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  4l_NikishinRoman
+//
+//  Created by Roman on 24.02.2020.
+//  Copyright © 2020 Roman Nikishin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
