36af213f4a5cdeb25ec77fbc3f7407e511cf76c7
Roman Nikishin <loudpiper@gmail.com>
2020-02-24 16:09:43 +0300

    Home Work partially done

--- 1 month/Lesson03/3l_NikishinRoman/3l_NikishinRoman/main.swift
+++ 1 month/Lesson03/3l_NikishinRoman/3l_NikishinRoman/main.swift
@@ -36,25 +36,43 @@ enum TrunkManipulations {
 
 enum CarState{
     case new
-    case used {var odo: Int}
+    case used(odo: Int)
 }
 
 struct Car {
     var brand: String
     var model: String
+    var year: Int
     var color: String
     var mileage: CarState
     var price: Int
+    
+    var trunkVolume: Double
+    private(set) var trunkLoadedVolume: Double
 
     private(set) var engineState: EngineState = .stop
+    private(set) var windowsState: WindowsState = .closed
     
 
-    init(brand: String, model: String, color: String, mileage: CarState = .new, price: Int){
+    init(brand: String,
+         model: String,
+         year: Int,
+         color: String,
+         mileage: CarState = .new,
+         price: Int,
+         trunkVolume: Double
+    ){
         self.brand = brand
         self.model = model
+        self.year = year
         self.color = color
         self.mileage = mileage
         self.price = price
+        self.trunkVolume = trunkVolume
+        
+        self.trunkLoadedVolume = 0.0
+        self.engineState = .stop
+        self.windowsState = .closed
     }
 
     mutating func changeEngineState(){
@@ -64,5 +82,79 @@ struct Car {
             engineState = .stop
         }
     }
+    mutating func changeWindowsState(){
+        if windowsState == .closed {
+            windowsState = .open
+        } else {
+            windowsState = .closed
+        }
+    }
+    mutating func manipulateWithTrunk(){
+        if windowsState == .closed {
+            windowsState = .open
+        } else {
+            windowsState = .closed
+        }
+    }
 }
-var car = Car(brand: "Audi", model: "A4", color: "black", mileage: .used(odo: 50_000), price: 1_000_000)
+struct Truck {
+    var brand: String
+    var model: String
+    var year: Int
+    var color: String
+    var mileage: CarState
+    var price: Int
+    
+    var trunkVolume: Double
+    private(set) var trunkLoadedVolume: Double
+
+    private(set) var engineState: EngineState = .stop
+    private(set) var windowsState: WindowsState = .closed
+    
+
+    init(brand: String,
+         model: String,
+         year: Int,
+         color: String,
+         mileage: CarState = .new,
+         price: Int,
+         trunkVolume: Double
+    ){
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
+        self.mileage = mileage
+        self.price = price
+        self.trunkVolume = trunkVolume
+        
+        self.trunkLoadedVolume = 0.0
+        self.engineState = .stop
+        self.windowsState = .closed
+    }
+
+    mutating func changeEngineState(){
+        if engineState == .stop {
+            engineState = .start
+        } else {
+            engineState = .stop
+        }
+    }
+    mutating func changeWindowsState(){
+        if windowsState == .closed {
+            windowsState = .open
+        } else {
+            windowsState = .closed
+        }
+    }
+//    mutating func manipulateWithTrunk(){
+//        if windowsState == .closed {
+//            windowsState = .open
+//        } else {
+//            windowsState = .closed
+//        }
+//    }
+}
+
+var car = Car(brand: "Audi", model: "A4", year: 2018, color: "black", mileage: .used(odo: 50_000), price: 1_000_000, trunkVolume: 480.0)
+var truck = Truck(brand: "Scania", model: "A200", year: 2014, color: "Red", mileage: .used(odo: 500_000), price: 9_000_000, trunkVolume: 50000.0)
