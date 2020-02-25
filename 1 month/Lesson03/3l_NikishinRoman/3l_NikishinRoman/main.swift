d97a4949e3008011823922e1e403731db6d3efd3
Roman Nikishin <loudpiper@gmail.com>
2020-02-25 13:42:07 +0300

    [Homework03] Full realization of Car enum

--- 1 month/Lesson03/3l_NikishinRoman/3l_NikishinRoman/main.swift
+++ 1 month/Lesson03/3l_NikishinRoman/3l_NikishinRoman/main.swift
@@ -7,9 +7,7 @@
 //
 
 /*
- 
- 1. Описать несколько структур – любой легковой автомобиль и любой грузовик.
- 2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+
  3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
  4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
  5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
@@ -20,6 +18,7 @@
 import Foundation
 
 
+
 enum EngineState {
     case start
     case stop
@@ -28,16 +27,26 @@ enum WindowsState {
     case open
     case closed
 }
-enum TrunkManipulations {
-    // TODO Реализовать метод указания количества груза
-    case load
-    case unload
-}
-
+//enum TrunkVolume {
+//    case empty
+//    case loaded(amount: Int)
+//}
 enum CarState{
     case new
     case used(odo: Int)
 }
+
+enum Actions {
+    case changeEngineState
+    case changeWindowsState
+    case loadTrunk(Int)
+    case unloadTrunk(Int)
+}
+
+/*
+ 1. Описать несколько структур – любой легковой автомобиль и любой грузовик.
+ 2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+ */
 
 struct Car {
     var brand: String
@@ -47,8 +56,8 @@ struct Car {
     var mileage: CarState
     var price: Int
     
-    var trunkVolume: Double
-    private(set) var trunkLoadedVolume: Double
+    private(set) var trunkVolume: Int
+    private(set) var trunkLoadedVolume: Int
 
     private(set) var engineState: EngineState = .stop
     private(set) var windowsState: WindowsState = .closed
@@ -60,7 +69,7 @@ struct Car {
          color: String,
          mileage: CarState = .new,
          price: Int,
-         trunkVolume: Double
+         trunkVolume: Int
     ){
         self.brand = brand
         self.model = model
@@ -70,31 +79,61 @@ struct Car {
         self.price = price
         self.trunkVolume = trunkVolume
         
-        self.trunkLoadedVolume = 0.0
+        self.trunkLoadedVolume = 0
         self.engineState = .stop
         self.windowsState = .closed
     }
+    
+    mutating func action (_ action: Actions){
+        switch action {
+        case .changeEngineState:
+            if engineState == .stop {
+                engineState = .start
+                print("Engine started")
+            } else {
+                engineState = .stop
+                print("Enngine stopped")
+            }
+        case .changeWindowsState:
+            if windowsState == .closed {
+                windowsState = .open
+                print("Windows opened")
+            } else {
+                windowsState = .closed
+                print("Windows closed")
+            }
+        case .loadTrunk(let loadingWeight):
+            if trunkVolume < loadingWeight {
+                print("Your luggage is larger than trunk!")
+            } else {
+                trunkLoadedVolume += loadingWeight
+                print("Free space left: \(trunkVolume - trunkLoadedVolume) liters")
+            }
+        case .unloadTrunk(let unloadedWeight):
+            if trunkLoadedVolume - unloadedWeight >= 0 {
+                trunkLoadedVolume -= unloadedWeight
+                print("Free space left: \(trunkVolume - trunkLoadedVolume) liters")
+            } else {
+                print("You cannot unload such amount, it's larger than \(trunkVolume - trunkLoadedVolume) liters left")
+            }
+        }
+    }
 
-    mutating func changeEngineState(){
-        if engineState == .stop {
-            engineState = .start
-        } else {
-            engineState = .stop
-        }
-    }
-    mutating func changeWindowsState(){
-        if windowsState == .closed {
-            windowsState = .open
-        } else {
-            windowsState = .closed
-        }
-    }
-    mutating func manipulateWithTrunk(){
-        if windowsState == .closed {
-            windowsState = .open
-        } else {
-            windowsState = .closed
-        }
+        func description() {
+            print("""
+                \n
+                Brand: \(brand)
+                Model: \(model)
+                Model year: \(year)
+                Color: \(color)
+                Current car conditions: \(mileage)
+                Car price \(price)
+                Current engine status: \(engineState)
+                Current windows status: \(windowsState)
+                Maximun trunk volume: \(trunkVolume)
+                Current trunk load: \(trunkLoadedVolume)\n
+                """)
+        
     }
 }
 struct Truck {
