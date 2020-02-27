f3ff3edd79ec0fef2b2d000181863dd28bdcbf71
Roman Nikishin <loudpiper@gmail.com>
2020-02-27 20:13:36 +0300

    [Homework05] Created empty project for 5th homework

---
+++ 1 month/Lesson05/5l_NikishinRoman/5l_NikishinRoman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  5l_NikishinRoman
+//
+//  Created by Admin on 27.02.2020.
+//  Copyright © 2020 Nikishin Roman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
