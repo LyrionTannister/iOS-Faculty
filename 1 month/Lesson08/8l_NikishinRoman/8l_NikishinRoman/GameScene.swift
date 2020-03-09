3acb012400abe4bd7b57c2f893a608b6de131055
Roman Nikishin <loudpiper@gmail.com>
2020-03-09 20:14:35 +0300

    [homework08] Template project created

---
+++ 1 month/Lesson08/8l_NikishinRoman/8l_NikishinRoman/GameScene.swift
@@ -0,0 +1,41 @@
+//
+//  GameScene.swift
+//  8l_NikishinRoman
+//
+//  Created by Roman on 09.03.2020.
+//  Copyright © 2020 Roman Nikishin. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+ 
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
