87f2edad5f7a3f38a360984b469f4454d3af5e4a
Roman Nikishin <loudpiper@gmail.com>
2020-03-13 21:59:05 +0300

    Initial commit

---
+++ 1 semester/Unit 2 - UI/1l_NikishinRoman/1l_NikishinRoman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1l_NikishinRoman
+//
+//  Created by Admin on 13.03.2020.
+//  Copyright © 2020 Nikishin Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
